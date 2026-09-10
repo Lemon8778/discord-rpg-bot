f93dd30e0e480ce68e3976c44d57facf7dfbddda
Lemon8778 <kennyang311@gmail.com>
2026-09-10 19:21:46 +0800

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,1 +1,18 @@
+import os
+from flask import Flask
+from threading import Thread
 
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is alive!"
+
+def run():
+    # 自動獲取 Render 給予的 PORT，預設為 10000
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
